9707792c8d450bc35f9d9599ae814403344f80ca
Cal Corum <calcorum@users.noreply.github.com>
2018-05-16 09:44:06 -0500

    Delete WeaponList.js

--- WeaponList.js
+++
@@ -1,4 +0,0 @@
-const WEAPONLIST = {
-    "assaultrifle":{"id":"assaultrifle","name":"Assault Rifle","damage":"2+1d3","critDamage":"2","noise":"Pew Pew!"},
-    "shotgun":{"id":"shotgun","name":"Shotgun","damage":"3+1d3","critChance":"15","critDamage":"2","noise":"KABOOM!"},
-}
