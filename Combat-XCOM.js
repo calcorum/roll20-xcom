d655cb71a97c2fd7a6bcff3fafe4cd2464cac7e8
Cal Corum <calcorum@users.noreply.github.com>
2018-05-18 00:10:57 -0500

    Update Combat-XCOM.js
    All XCOM specific listeners were added to this extension of the Combat engine.

--- Combat-XCOM.js
+++ Combat-XCOM.js
@@ -1,29 +1,56 @@
-// Assigning hit points and ACs to a newly assigned token
-on("change:graphic:represents", function(token){
+/*jshint esversion: 6 */
+
+/**
+ * Called by Combat-CORE.importCharacter to support any updates following Attributes
+ * @param  Roll20 Character Object  char the character to be updated
+ * @return None
+ */
+function importGameSpecifics(char){
+    return;
+}
+
+/**
+ * Update stats on token when the "represents" tag is updated
+ * @param  Roll20 Token Object  token
+ * @return none
+ */
+function updateToken(token){
     let char = getObj("character", token.get("represents")) || null;
     if(char){
         let hitpoints = getAttribute(char, "attribute-health") || null;
         if(hitpoints){
+            let ablative = getAttribute(char, "gear-ablative") || 0;
             token.set({
-                bar2_value: hitpoints,
+                bar2_value: hitpoints + ablative,
                 bar2_max: hitpoints,
                 showplayers_bar3: true,
                 showplayers_name: true,
                 showname: true,
             });
         
-            let bMove = MOBILITY[getAttribute(char, "attribute-mobility")]["blue"] || null;
-            let yMove = MOBILITY[getAttribute(char, "attribute-mobility")]["yellow"] || null;
+            let bMove = MOBILITY[getAttribute(char, "attribute-mobility")].blue || null;
+            let yMove = MOBILITY[getAttribute(char, "attribute-mobility")].yellow || null;
             // Set bar 1 to eac
             if(bMove) token.set("bar3_value", bMove);
             // Set bar 2 to kac
             if(yMove) token.set("bar1_value", yMove);
         }
     }
+}
+
+// Assigning hit points and ACs to a newly assigned token
+on("change:graphic:represents", function(token){
+    updateToken(token);
 });
 
+// Accepting the api commands for XCOM
 on("chat:message", function(msg){
     if(msg.type === "api" && msg.content.match(/^!attack/)){
+
+        //////////////////////////
+        // API COMMAND: !attack //
+        //////////////////////////
+
         let attacker = null;
         let target = null;
         let attackerToken = null;
@@ -31,6 +58,7 @@ on("chat:message", function(msg){
         let weapon = null;
         let cover = 0;
         let rangeMod = 0;
+        let overwatch = 0;
         let input = null;
         
         if(LOGLEVEL > 2) log("Combat-XCOM / main / msg.content: " + msg.content);
@@ -45,14 +73,14 @@ on("chat:message", function(msg){
         
         // Process each of the parameters
         for(param in params){
-            if(LOGLEVEL > 2) log("Weapons / params / param: " + param);
+            if(LOGLEVEL > 2) log("Combat-XCOM / params / param: " + param);
             switch(param){
                 case "attacker":
                     attacker = getChar(params[param]);
                     if(attacker){
-                        if(LOGLEVEL > 2) log("Weapons / params / attacker: " + attacker.get("name"));
+                        if(LOGLEVEL > 2) log("Combat-XCOM / params / attacker: " + attacker.get("name"));
                     }else{
-                        if(LOGLEVEL > 0) log("Weapons / params / Invalid attacker");
+                        if(LOGLEVEL > 0) log("Combat-XCOM / params / Invalid attacker");
                         sendMessage(getChar("Clippy"),msg.who,"Error Code: Blue Rabbit");
                         return;
                     }
@@ -60,9 +88,9 @@ on("chat:message", function(msg){
                 case "target":
                     target = getChar(params[param]);
                     if(target){
-                        if(LOGLEVEL > 2) log("Weapons / params / target: " + target.get("name"));
+                        if(LOGLEVEL > 2) log("Combat-XCOM / params / target: " + target.get("name"));
                     }else{
-                        if(LOGLEVEL > 0) log("Weapons / params / Invalid target");
+                        if(LOGLEVEL > 0) log("Combat-XCOM / params / Invalid target");
                         sendMessage(getChar("Clippy"),msg.who,"Error Code: Yellow Chinchilla");
                         return;
                     }
@@ -70,127 +98,85 @@ on("chat:message", function(msg){
                 case "weapon":
                     weapon = WEAPONLIST[params[param].toLowerCase()] || null;
                     if(weapon){
-                        if(LOGLEVEL > 2) log("Weapons / params / weapon: " + weapon.name);
+                        if(LOGLEVEL > 2) log("Combat-XCOM / params / weapon: " + weapon.name);
                     }else{
-                        if(LOGLEVEL > 0) log("Weapons / params / Invalid weapon");
+                        if(LOGLEVEL > 0) log("Combat-XCOM / params / Invalid weapon: " + params[param]);
                         sendMessage(getChar("Clippy"),msg.who,"Error Code: Green Tabby");
                         return;
                     }
                     break;
                 case "cover":
-                    value = params[param];
-                    if(parseInt(value) || value === "0"){ 
-                        cover = value;
-                        if(LOGLEVEL > 2) log("Weapons / params / cover: " + cover);
+                    cover = params[param];
+                    if(cover || cover == 0){
+                        if(LOGLEVEL > 2) log("Combat-XCOM / params / cover: " + cover);
                     }else{
-                        if(LOGLEVEL > 0) log("Weapons / params / Invalid cover");
+                        if(LOGLEVEL > 0) log("Combat-XCOM / params / Invalid cover: " + params[param]);
                         sendMessage(getChar("Clippy"),msg.who,"Error Code: Silver Anteater");
                         return;
                     }
                     break;
                 case "attackerToken":
-                    value = params[param];
-                    tObj = getObj('graphic',value);
-                    if(tObj){
-                        attackerToken = tObj;
-                        if(LOGLEVEL > 2) log("Weapons / params / attacker token found");
+                    attackerToken = getObj('graphic',params[param]) || null;
+                    if(attackerToken){
+                        if(LOGLEVEL > 2) log("Combat-XCOM / params / attacker token found");
                     }else{
-                        if(LOGLEVEL > 0) log("Weapons / params / could not find attacker token");
+                        if(LOGLEVEL > 0) log("Combat-XCOM / params / could not find attacker token");
                         sendMessage(getChar("Clippy"),msg.who,"Error Code: Navy Platypus");
                         return;
                     }
                     break;
                 case "targetToken":
-                    value = params[param];
-                    tObj = getObj('graphic',value);
-                    if(tObj){
-                        targetToken = tObj;
-                        if(LOGLEVEL > 2) log("Weapons / params / target token found");
+                    targetToken = getObj('graphic', params[param]) || null;
+                    if(targetToken){
+                        if(LOGLEVEL > 2) log("Combat-XCOM / params / target token found");
                     }else{
-                        if(LOGLEVEL > 0) log("Weapons / params / could not find target token");
+                        if(LOGLEVEL > 0) log("Combat-XCOM / params / could not find target token");
                         sendMessage(getChar("Clippy"),msg.who,"Error Code: Pink Tabby");
                         return;
                     }
                     break;
+                case "overwatch":
+                    overwatch = params[param];
+                    if(overwatch || overwatch == 0){
+                        if(LOGLEVEL > 2) log("Combat-XCOM / params / overwatch: " + overwatch);
+                    }
+                    break;
                 default:
-                    if(LOGLEVEL > 0) log("Weapons / params / Unrecognized parameter");
+                    if(LOGLEVEL > 0) log("Combat-XCOM / params / Unrecognized parameter");
                     sendMessage(getChar("Clippy"),msg.who,"Error Code: Mauve Toad");
                     break;
             }
         }
         
-        // Parameter Checks
-        if(!attacker) return;
-        if(!target) return;
-        if(!weapon) return;
-        if(!attackerToken) return;
-        if(!targetToken) return;
-        
         // ---------------------------------------------------------
         
         // Determine attacker's Aim
-        // Check base aim
-        let attackerAimBase = 0;
-        let tempAim = getAttribute(attacker,"attribute-aim");
-        if(parseInt(tempAim)) attackerAimBase = tempAim;
-        else{
-            if(LOGLEVEL > 0) log("Weapons / main / attacker has no Aim");
-            sendMessage(getChar("Clippy"),msg.who,"Error Code: Pink Hippo");
-        }
-        if(LOGLEVEL > 2) log("Weapons / main / attacker base aim: " + attackerAimBase);
-        
-        // Check for Aim Bonus
-        let attackerAimBonus = 0;
-        let tempAimBonus = getAttribute(attacker,"temp-aimBonus");
-        if(parseInt(tempAimBonus)) attackerAimBonus = tempAimBonus;
-        if(LOGLEVEL > 2) log("Weapons / main / attacker aim bonus: " + tempAimBonus);
-        
-        // Check for Aim Penalty
-        let attackerAimPenalty = 0;
-        let tempAimPenalty = getAttribute(attacker,"temp-aimPenalty");
-        if(parseInt(tempAimPenalty)) attackerAimPenalty = tempAimPenalty;
-        if(LOGLEVEL > 2) log("Weapons / main / attacker aim penalty: " + tempAimPenalty);
-        
-        // Calculate adjusted Aim Value
-        let attackerAim = attackerAimBase + attackerAimBonus - attackerAimPenalty;
-        if(LOGLEVEL > 2) log("Weapons / main / attacker adjusted aim: " + attackerAim);
-        
-        // Determine Target's Defense
-        // Check base defense
-        let targetDefenseBase = 0;
-        let tempDefenseBase = getAttribute(target,"attribute-defense");
-        if(parseInt(tempDefenseBase)) targetDefenseBase = tempDefenseBase;
-        if(LOGLEVEL > 2) log("Weapons / main / target base defense: " + targetDefenseBase);
-        
-        // Check for Defense Bonus
-        let targetDefenseBonus = 0;
-        let tempDefenseBonus = getAttribute(target,"temp-defenseBonus");
-        if(parseInt(tempDefenseBonus)) targetDefenseBonus = tempDefenseBonus;
-        if(LOGLEVEL > 2) log("Weapons / main / target defense bonus: " + targetDefenseBonus);
-        
-        // Check for Defense Penalty
-        let targetDefensePenalty = 0;
-        let tempDefensePenalty = getAttribute(attacker,"temp-defensePenalty");
-        if(parseInt(tempDefensePenalty)) targetDefensePenalty = tempDefensePenalty;
-        if(LOGLEVEL > 2) log("Weapons / main / target defense penalty: " + targetDefensePenalty);
-        
-        // Calculate adjusted Defense Value
-        let targetDefense = targetDefenseBase + targetDefenseBonus - targetDefensePenalty;
-        if(LOGLEVEL > 2) log("Weapons / main / Target adjusted defense: " + targetDefense);
-        
+        let aimResp = getAttrSums(
+            [
+                [attacker,"attribute-aim"],
+                [attacker,"temp-aimBonus"],
+                [target,"temp-defensePenalty"]
+            ],
+            [
+                [attacker,"temp-aimPenalty"],
+                [target,"attribute-defense"],
+                [target,"temp-defenseBonus"]
+            ]
+        );
+
         // ---------------------------------------------------------
         // Check distance
         let distance = distBetween(attackerToken, targetToken);
         let rMod = weapon.range[distance];
-        if(LOGLEVEL > 2) log("Weapons / main / rMod: " + rMod);
-        if(parseInt(rMod)) rangeMod = rMod;
+        //rangeMod = weapon.range[distance] || weapon.range.other;
+        if(LOGLEVEL > 2) log("Combat-XCOM / main / rMod: " + rMod);
+        if(parseInt(rMod) || rMod == 0) rangeMod = rMod;
         else rangeMod = weapon.range.other;
-        if(LOGLEVEL > 2) log("Weapons / main / rangeMod: " + rangeMod);
+        if(LOGLEVEL > 2) log("Combat-XCOM / main / rangeMod: " + rangeMod);
         
-        // ---------------------------------------------------------
-        
+        // ---------------------------------------------------------        
         // Check for a hit
-        let toHitChance = parseInt(attackerAim - targetDefense - cover + rangeMod);
+        let toHitChance = aimResp.vals.total + rangeMod - cover - overwatch;
         let toHitDie = randomInteger(100);
         let damage = 0;
         let critChance = parseInt(0);
@@ -198,8 +184,9 @@ on("chat:message", function(msg){
         let weaponCrit = parseInt(0);
         let flankCrit = parseInt(0);
         
-        if(LOGLEVEL > 1) log("Weapons / main / toHitChance: " + toHitChance);
-        if(LOGLEVEL > 1) log("Weapons / main / toHitDie: " + toHitDie);
+        if(LOGLEVEL > 1) log("Combat-XCOM / main / cover: " + cover);
+        if(LOGLEVEL > 1) log("Combat-XCOM / main / toHitChance: " + toHitChance);
+        if(LOGLEVEL > 1) log("Combat-XCOM / main / toHitDie: " + toHitDie);
         
         let outcome = "Miss";
         
@@ -207,16 +194,16 @@ on("chat:message", function(msg){
         if(parseInt(weapon.critChance)) weaponCrit += weapon.critChance;
         if(cover == 0) flankCrit = 40;
         critChance = parseInt(weaponCrit) + parseInt(flankCrit);
-        if(LOGLEVEL > 1) log("Weapons / main / critChance: " + critChance);
+        if(LOGLEVEL > 1) log("Combat-XCOM / main / critChance: " + critChance);
         
         if(toHitDie <= toHitChance) { 
             damage = weapon.damage;
             outcome = "HIT";
             
             // Check for crit
-            if(critChance > 0){
+            if(critChance > 0 && params.overwatch == null){
                 critDie = randomInteger(100);
-                if(LOGLEVEL > 1) log("Weapons / main / critDie: " + critDie);
+                if(LOGLEVEL > 1) log("Combat-XCOM / main / critDie: " + critDie);
                 if(critDie <= critChance){
                     damage += "+" + parseInt(weapon.critDamage);
                     outcome = "CRIT";
@@ -228,16 +215,61 @@ on("chat:message", function(msg){
             "{{Target=" + targetToken.get("name") + "}} {{Hit Chance=" + toHitChance + "%}} " + 
             "{{Hit Roll=" + toHitDie + "}} " + "{{Crit Chance=" + critChance + "%}} {{Crit Roll=" + critDie + "}}";
             
-        if(PLAYERLOG) sendMessage(getChar("Clippy"),msg.who,piString);
+        if(PLAYERLOG) sendMessage(getChar("Clippy"),msg.who,piString,true);
         
         if(outcome !== "Miss"){
             outcome += " for [[" + damage + "]] !";
         }
         
-        sendMessage(attacker,null,attacker.get("name") + " fires the " + weapon.name + " at the " + target.get("name") + "!");
+        sendMessage(attacker,null,attacker.get("name").split(" ")[0] + " fires the " + weapon.name + " at the " + target.get("name") + "!");
         sendMessage(attacker,null,weapon.noise);
         sendMessage(attacker,null,outcome);
         
         if(LOGLEVEL > 0) log("Combat-XCOM / main / END ATTACK ------------------------------");
+    
+    }else if(msg.type === "api" && msg.content.match(/^!updateToken/)){
+
+        ///////////////////////////////
+        // API COMMAND: !updateToken //
+        ///////////////////////////////
+
+        // Parse the message content for parameters
+        let paramResp = getParameters(msg.content, "!updateToken ");
+        let params = null;
+        if(paramResp.success){
+            params = paramResp.vals;
+        }else{
+            sendMessage(getChar("Clippy"),msg.who,paramResp.error, true);
+        }
+
+        // get tokenID
+        let tokenId = params.tokenid;
+        let token = getObj("graphic",tokenId);
+        if(LOGLEVEL > 2) log("Combat-XCOM / updateToken / tokenId: " + tokenId);
+        if(LOGLEVEL > 2) log("Combat-XCOM / updateToken / token: " + token);
+
+        // update token
+        updateToken(getObj("graphic", tokenId));
+        if(LOGLEVEL > 1) log("Combat-XCOM / updateToken / Successfully updated token id: " + tokenId);
+
+    }else if(msg.type === "api" && msg.content.match(/^!overwatch/)){
+
+        /////////////////////////////
+        // API COMMAND: !overwatch //
+        /////////////////////////////
+
+        // TODO: DO STUFF FOR OVERWATCH!
+        
+        // GET CHARACTER
+        
+        // SET OVERWATCH STATUS ICON
+        
+        // REMOVE STANDARD ABILITIES FROM TOKEN ACTION
+        
+        // SET OVERWATCH SHOT ABILITY TO TOKEN ACTION
+        
+        log("Combat-XCOM / overwatch / I'M HERE!!!");
+        sendMessage(getChar(),null,"!attack boobs");
     }
+
 });
