89354a705f609a42d29be244549c38de43b4e9fb
Cal Corum <calcorum@users.noreply.github.com>
2018-05-18 00:12:25 -0500

    Update XcomData.js
    Moved all constant variables out of the engine extension and into the data file. This contains constants for both Combat-CORE and Combat-XCOM

--- XcomData.js
+++ XcomData.js
@@ -1,3 +1,13 @@
+/*jshint esversion: 6 */
+
+const CHARGENATTRS = [["attribute-health",3,3],["attribute-aim",65,null],["attribute-defense",0,null],["attribute-will",25,null],["attribute-hacking",1,null],["attribute-mobility",11,null],["gear-priWeapon","assaultrifle",null]];
+const CHARGENABILS = [["Import-Stats","!import charId=@{character_id}",false],["Update-Token","!updateToken tokenId=@{selected|tokenId}",false]];
+const PLAYERMAP = {};
+const LOGLEVEL = 3;
+const AUTOHIT = false;
+const AUTOCRIT = false;
+const PLAYERLOG = true;
+
 const RIFLERANGES = {1:30,2:27,3:24,4:21,5:18,6:15,7:12,8:9,9:6,10:3,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:-5,21:-10,22:-15,23:-20,24:-25,"other":-30};
 const SHOTGUNRANGES = {1:60,2:45,3:30,4:15,5:8,6:4,7:0,8:0,9:-4,10:-8,11:-16,12:-32,13:-40,14:-48,15:-60,16:-70,17:-80,18:-90,"other":-100};
 const CANNONRANGES = {1:-10,2:-5,3:0,4:5,5:10,6:8,7:6,8:4,9:2,"other":0};
@@ -6,17 +16,22 @@ const SAWEDOFFRANGES = {1:60,2:30,4:-20,5:-40,6:-80,"other":-100};
 
 const WEAPONLIST = {
     "assaultrifle":{"id":"assaultrifle","name":"Assault Rifle","damage":"2+1d3","critDamage":"2","noise":"Pop Pop Pop!","range":RIFLERANGES},
-    "shotgun":{"id":"shotgun","name":"Shotgun","damage":"3+1d3","critChance":"15","critDamage":"2","noise":"KABOOM!","range":SHOTGUNRANGES},
+    "shotgun":{"id":"shotgun","name":"Shotgun","damage":"3+1d3","critChance":"15","critDamage":"2","noise":"THOOM!","range":SHOTGUNRANGES},
     "beamrifle":{"id":"beamrifle","name":"Beam Rifle","damage":"2+1d3","critDamage":"2","noise":"Feeyoo Feeyoo!","range":RIFLERANGES},
     "adventrifle":{"id":"adventrifle","name":"Advent Rifle","damage":"1+1d3","critDamage":"2","noise":"Pew Pew!","range":RIFLERANGES},
-    "sawedoffshotgun":{"id":"sawedoffshotgun","name":"Sawed-Off Shotgun","damage":"4+1d5","critChance":"15","critDamage":"2","noise":"KABOOM!","range":SAWEDOFFRANGES},
+    "adventmagrifle":{"id":"adventmagrifle","name":"Advent Mag Rifle","damage":"3+1d2","critDamage":"2","noise":"Pew Pew!","range":RIFLERANGES},
+    "sawedoffshotgun":{"id":"sawedoffshotgun","name":"Sawed-Off Shotgun","damage":"4+1d5","critChance":"15","critDamage":"2","noise":"THOOM!","range":SAWEDOFFRANGES},
     "smg":{"id":"smg","name":"Submachine Gun","damage":"1+1d3","critDamage":"1","noise":"Pop Pop Pop!","range":SMGRANGES},
+};
+
+const GRENADELIST = {
+    "frag":{"id":"frag","name":"Frag Grednade","maxDamage":5,"minDamage":2,"diameter":3,"noise":"KABOOM!"}
 };
 
 const ENEMYRANDSTATS = {
     "mk1trooper":{"attribute-health":[3,4],"attribute-will":[40,45,45,45]},
     "sectoid":{"attribute-health":[7,8,8,9], "attribute-will":[80,85,05,05], "attribute-aim":[70,75,75,75], "bonus-crit":[0,0,10,10], "attribute-psiOffense":[80,90,90,90]},
-}
+};
 
 const MOBILITY = {
     1:{"blue":1,"yellow":1},
